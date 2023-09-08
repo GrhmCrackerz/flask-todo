e0a7b3bb9982171fee2a08c34458fecab195cb49
GrhmCrackerz <88797817+GrhmCrackerz@users.noreply.github.com>
2023-09-08 14:29:33 -0400

    Complete App from video tutorial

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -18,8 +18,33 @@ class Todo(db.Model):
 def index():
     #show all todos on index page
     todo_list = Todo.query.all()
-    print(todo_list)
+    #print(todo_list)
     return render_template('base.html', todo_list=todo_list)
+
+@app.route("/add", methods=["POST"])
+def add():
+    # add new item
+    title = request.form.get("title")
+    new_todo = Todo(title=title, complete=False) # creates new item for todo list, set initially to false
+    db.session.add(new_todo) # adds item to database
+    db.session.commit() # sends/commits the information to the database
+    return redirect(url_for("index")) # after adding something to the list, redirect to the "index" url to refresh page
+
+@app.route("/update/<int:todo_id>")
+def update(todo_id):
+    # update an existing item
+    todo = Todo.query.filter_by(id=todo_id).first() # we query through our list/db by the todo_id, we only need to return the first one
+    todo.complete = not todo.complete # we set this to the opposite so that we can go back to either complete or not complete
+    db.session.commit() # sends/commits the information to the database
+    return redirect(url_for("index")) # after adding something to the list, redirect to the "index" url to refresh page
+
+@app.route("/delete/<int:todo_id>")
+def delete(todo_id):
+    # delete an existing item
+    todo = Todo.query.filter_by(id=todo_id).first() # we query through our list/db by the todo_id, we only need to return the first one
+    db.session.delete(todo)
+    db.session.commit() # sends/commits the information to the database
+    return redirect(url_for("index")) # after adding something to the list, redirect to the "index" url to refresh page
 
 
 if __name__ == "__main__":
