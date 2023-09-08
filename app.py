c84baf8fa792cf7abd5fddba79ab73c5df89ba27
GrhmCrackerz <88797817+GrhmCrackerz@users.noreply.github.com>
2023-09-08 11:27:06 -0400

    Additional changes

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.app_context().push()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app) 
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(100)) #max amount of characters is 100
+    complete = db.Column(db.Boolean) #used to check if a task was completed
+
 
 @app.route('/')
 def index():
-    return "Hello World"
+    #show all todos on index page
+    todo_list = Todo.query.all()
+    print(todo_list)
+    return render_template('base.html', todo_list=todo_list)
+
+
+if __name__ == "__main__":
+    db.create_all()
+
+    app.run(debug=True)
